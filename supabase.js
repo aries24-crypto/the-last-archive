2b02d38c666fd40a85109994e7ed73521840a910
aries24-crypto <tejas.ydv24@gmail.com>
2026-05-08 15:54:38 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
+
 const supabaseUrl = 'https://xvjoadlvestpektltwjy.supabase.co'
+
 const supabaseKey = 'sb_publishable__5IOXMaW1dvb2hzdhh6O3g__4ERwbPP'
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   supabaseUrl,
   supabaseKey
 )
-
